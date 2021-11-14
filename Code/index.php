1d9c343e2ee7b629613adb8327949491e8cbbce6
luc <80699609+LucLeneuf@users.noreply.github.com>
2021-11-14 21:01:39 +0100

    up

---
+++ Code/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/front_page.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+        <div class="logo"></div>
+        
+        <div class="menu_computer">
+                <a href="#"> ipsum </a>
+                <a href="#"> ipsum </a>
+                <a href="#"> ipsum </a>
+        </div>
+
+        <div class="menu_mobile">
+                <div class="menu_mobile__items" id="menu">
+                    <a href="#"> ipsum </a>
+                    <a href="#"> ipsum </a>
+                    <a href="#"> ipsum </a>
+                </div>
+
+                <div class="menu_mobile__button" id="button_mobile">
+                    <div class="menu_mobile__button__hamburger" id="hamburger">
+                        <div></div>
+                        <div></div>
+                    </div>
+
+                    <div class="menu_mobile__button__cross" id="cross">x</div>
+                </div>
+        </div>
+    </header>
+
+    <script src="assets/scripts/burger.js"></script>
+</body>
+
+</html>
